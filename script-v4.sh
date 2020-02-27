3a12fa1bd1c98db8951f1e2cda5038466c8885a6
root <root@vps785418.ovh.net>
2020-02-27 16:34:16 +0100

    ajout script

---
+++ script-v4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "update de mariadb"
+apt update
+apt install mariadb-server mariadb-client
+echo "clean du cache"
+apt clean
+echo " upadate de apache 2"
+apt update
+apt install apache2 libapache2-mod-php7.0
+echo "clean du cache"
+apt clean
+mkdir -p /var/www/$1
+echo " fin du script" 
+projet="$1"
+echo $projet
